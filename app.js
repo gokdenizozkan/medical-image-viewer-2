fddbfbc858167fb2de162cc29bae7ddf8f552ac9
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2025-07-17 23:33:47 +0300

    initialize ZoomNPan

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+ const viewer = document.querySelector('.miv2-image-display');
+ const gallery = document.querySelector('.miv2-image-gallery');
  
+ const zoomNPan = new ZoomNPan(viewer);
+ zoomNPan.minScale = 50;
